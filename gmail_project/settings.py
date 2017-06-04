694046e2f12f5b8d8a41407ab3461f8c0381737c
Korsunow Konstantin <korsunowk@yandex.ua>
2017-06-04 10:27:23 +0300

    Update settings.py

--- gmail_project/settings.py
+++ gmail_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'rest_framework',
     'gmail_project',
     'oauth2_provider',
